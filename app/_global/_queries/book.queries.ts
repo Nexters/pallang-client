42e37e883f32bb653f21432689f55d633bda158b
hae-on <solbi2004@naver.com>
2026-07-28 22:00:02 +0900

    feat: 책 캐러셀, 책 내부 검색 infinite query 로직 구현

--- app/_global/_queries/book.queries.ts
+++ app/_global/_queries/book.queries.ts
@@ -1,12 +1,29 @@
-import { queryOptions } from '@tanstack/react-query'
+import { infiniteQueryOptions } from '@tanstack/react-query'
 
-import { getHomeCarouselBooks } from '../_apis/_generated/book/book'
+import { getHomeCarouselBooks, searchInternalBooks } from '../_apis/_generated/book/book'
+import type { GetHomeCarouselBooksParams } from '../_apis/_generated/models/getHomeCarouselBooksParams'
+import type { SearchInternalBooksParams } from '../_apis/_generated/models/searchInternalBooksParams'
 
 export const bookQueries = {
   all: () => ['book'] as const,
-  homeCarousel: () =>
-    queryOptions({
-      queryKey: [...bookQueries.all(), 'home-carousel'],
-      queryFn: () => getHomeCarouselBooks(),
+  homeCarousel: (params?: Omit<GetHomeCarouselBooksParams, 'page'>) =>
+    infiniteQueryOptions({
+      queryKey: [...bookQueries.all(), 'home-carousel', params],
+      queryFn: ({ pageParam }) => getHomeCarouselBooks({ ...params, page: pageParam }),
+      initialPageParam: 0,
+      getNextPageParam: (lastPage) => {
+        const pageInfo = lastPage.data?.pageInfo
+        return pageInfo?.hasNext ? pageInfo.page + 1 : undefined
+      },
+    }),
+  searchInternal: (params: Omit<SearchInternalBooksParams, 'page'>) =>
+    infiniteQueryOptions({
+      queryKey: [...bookQueries.all(), 'internal-search', params],
+      queryFn: ({ pageParam }) => searchInternalBooks({ ...params, page: pageParam }),
+      initialPageParam: 0,
+      getNextPageParam: (lastPage) => {
+        const pageInfo = lastPage.data?.pageInfo
+        return pageInfo?.hasNext ? pageInfo.page + 1 : undefined
+      },
     }),
 }
