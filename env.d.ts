089dfc4aae387ebb3e8877e6170c39b045e71911
hae-on <solbi2004@naver.com>
2026-08-08 21:52:38 +0900

    feat: 구글 애널리틱스 적용

--- env.d.ts
+++ env.d.ts
@@ -2,5 +2,7 @@ declare namespace NodeJS {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- ProcessEnv 선언 병합에는 interface가 필요
   interface ProcessEnv {
     readonly NEXT_PUBLIC_API_URL?: string
+    readonly NEXT_PUBLIC_GA_MEASUREMENT_ID?: string
+    readonly VERCEL_ENV?: 'development' | 'preview' | 'production'
   }
 }
