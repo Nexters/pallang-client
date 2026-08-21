6a909ff67fe821d4d17b6f8641b5161b00240fd4
hae-on <solbi2004@naver.com>
2026-08-22 01:33:21 +0900

    fix: 선택 리본 제목 겹침 방지

--- app/_shared/book/_components/ExternalBookList/ExternalBookList.tsx
+++ app/_shared/book/_components/ExternalBookList/ExternalBookList.tsx
@@ -89,9 +89,11 @@ export function ExternalBookList({
                 >
                   <BookItem
                     author={book.author}
+                    className="pr-12"
                     coverImageUrl={book.coverImageUrl}
                     publisher={book.publisher}
                     title={book.title}
+                    titleBehavior="clamp"
                   />
                   {isSelected && <BookSelectRibbon />}
                 </button>
